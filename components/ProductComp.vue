88e6336602dad5ad3d4cfcaf7e971782d335108e
cccmmmd <bblisme8b@gmail.com>
2024-06-02 13:54:38 +0800

    index page, loading, product card, get product api, add dark mode

--- components/ProductComp.vue
+++ components/ProductComp.vue
@@ -31,23 +31,24 @@
                 <img class="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
             </NuxtLink>
             <div class="px-5 pb-5">
+                <span class="text-3xl font-bold text-gray-900 dark:text-white">{{ product.price }}</span>
+                <span v-else class="text-3xl font-bold text-gray-900 dark:text-white line-through">Original price: {{ product.price * 1.5 }}</span>
                 <NuxtLink :to="`product-${product.id}`">
                     <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ product.title }}</h5>
                 </NuxtLink>
                 <NuxtLink :to="`product-${product.id}`">
-                    <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ product.description }}</h5>
+                <p class="mb-3 text-gray-500 dark:text-gray-400 truncate">{{ product.description }}</p>
                 </NuxtLink>
                 <div class="flex items-center mt-2.5 mb-5">
                    <h5> Rating: </h5>
                     <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{{ getRandomRating }}</span>
                 </div>
                 <div class="flex items-center justify-between">
-                    <span class="text-3xl font-bold text-gray-900 dark:text-white">{{ product.price }}</span>
                     <button @click="addToCart(product)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <span v-if="alreadyInCart(product)" class="text-3xl font-bold text-gray-900 dark:text-white">Item Added</span>
                         <span v-else class="text-3xl font-bold text-gray-900 dark:text-white">Add to Cart</span>
                     </NuxtLink>
-                    <span v-else class="text-3xl font-bold text-gray-900 dark:text-white line-through">Original price: {{ product.price * 1.5 }}</span>
+                    
                 </div>
             </div>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
-<script lang="ts" setup>
-
+<script setup>
+    const {data: products} = await useFetch('/api/products')
 </script>
 
 <template>
-    
+    <div class="grid grid-cols-4 gap-4">
+        <div v-for="pro in products">
+            <PtoductComp :product = "pro" />
+        </div>
+    </div>
 </template>
