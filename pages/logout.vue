be69cce02c7e00220af99ed1f03cd6c08bfc5585
Barbie Lee <137893455+cccmmmd@users.noreply.github.com>
2024-06-04 00:52:52 +0800

    add github & google login

--- pages/logout.vue
+++ pages/logout.vue
@@ -14,7 +14,7 @@
         setTimeout(async () => {
             successMsg.value = null
             await navigateTo('/')
-        }, 2000)
+        }, 1000)
 
     }
 
@@ -31,7 +31,7 @@
 <template>
     <div class="p-20 text-center">
         <h1 class="mb-[60px] text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Are you sure you wanna logout?</h1>
-        <NuxtLink @click="logout()" class="mb-12  md:mr-10  inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-800 dark:text-white rounded-lg hover:text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900  md:mb-20">
+        <NuxtLink @click="logout()" class=" cursor-pointer mb-12  md:mr-10  inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-800 dark:text-white rounded-lg hover:text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900  md:mb-20">
             Yes, log me out
             <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
