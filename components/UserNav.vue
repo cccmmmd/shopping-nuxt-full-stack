d81ef86ec7fc74c79575e4ce3b6885b7c8abf1fb
Barbie Lee <137893455+cccmmmd@users.noreply.github.com>
2024-06-08 16:01:02 +0800

    update page structure

--- components/UserNav.vue
+++ components/UserNav.vue
@@ -28,12 +28,9 @@
                 </span>
                 </div>
                 <ul class="py-2" aria-labelledby="user-menu-button">
-                <li>
-                    <NuxtLink to="/account" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">My Account</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Logout</NuxtLink>
-                </li>
+                    <li>
+                        <NuxtLink to="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Logout</NuxtLink>
+                    </li>
                 </ul>
             </div>
             <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
